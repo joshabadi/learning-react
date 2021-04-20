6ec9051ae7e437585ddf8124f03a789e0f5866bd
Josh <joshabadi@gmail.com>
2021-04-20 18:34:42 -0400

    added untracked files

---
+++ to-do/src/components/App.Styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const PurpleButton = styled.input`
+  background-color: rgb(89, 64, 119);
+  padding: 10px 0;
+  width: 300px;
+  color: #f4f6f8;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgb(109, 84, 140);
+  }
+`;
