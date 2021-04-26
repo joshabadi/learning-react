b17a9834b334edde73e664d40badafb9f152e016
Josh <joshabadi@gmail.com>
2021-04-26 18:44:45 -0400

    code review fixes

--- to-do/src/components/Todo.tsx
+++ to-do/src/components/Todo.tsx
@@ -33,7 +33,7 @@ const Todo = ({
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
 
-  const handleClick = (event: any) => {
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
 
